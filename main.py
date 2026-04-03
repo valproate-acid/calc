cb19e71e46534eec8de07560fbfbc1b51b7c338d
woohung123 <hyeongjunu97@gmail.com>
2026-04-03 16:45:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 def main():
     print("Hello from calc!")
 
+def mul(a, b):
+    return a*b
+
+def div(a, b):
+    if b == 0:
+        return "error"
+    return a/b
+
 def add(num1,num2):
     return num1+num2
 def minus(num1,num2):
